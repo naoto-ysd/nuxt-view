df2bbc66df5991df6fec514803d6f1361aef94b9
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2022-11-13 22:42:41 +0900

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-view',
+    title: 'Hello Nuxt.js',
     htmlAttrs: {
       lang: 'en'
     },
